829dd6d3cd2b16dd87acb25a2821b55454997297
nidhu.gopal <nidhu.gopal.p@gmail.com>
2019-11-14 15:45:57 -0800

    Add project files.

---
+++ apiConnection/Program.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace apiConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string BASE_URL = "https://swapi.co/api/";
+            const string PLANETS = "planets/";
+            string url = BASE_URL + PLANETS;
+            //string url = "https://swapi.co/api/planets/";
+
+            while (true)
+            {
+                JObject planetdata = CallRestMethod(url);
+
+                JArray planets = (JArray)planetdata["results"];
+                foreach (JObject planet in planets)
+                {
+                    Console.WriteLine(planet["name"]);
+                    JArray films = (JArray)planet["films"];
+                    foreach (JValue film in films)
+                    {
+                        Console.WriteLine(CallRestMethod(film.ToString())["title"]);
+                    }
+                    Console.WriteLine();
+                }
+
+                if (planetdata["next"].Type != JTokenType.Null)
+                {
+                    url = planetdata["next"].ToString();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static JObject CallRestMethod(string url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri(url));
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader responseStream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                return JObject.Parse(responseStream.ReadToEnd());
+            }
+            catch (Exception e)
+            {
+                return JObject.Parse($"{{'Error':'{e.Message}'}}");
+            }
+        }
+    }
+}
